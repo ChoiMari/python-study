35f2a0e1981cd9e4c288b0d5305f972cadb727ba
ChoiMari <chl98639863@gmail.com>
2025-08-09 17:24:46 +0900

    pizza order 실습

---
+++ day3/pizza_order.py
@@ -0,0 +1,38 @@
+# 피자 주문 실습
+s_pizza_size: int = 15
+m_pizza_size: int = 20
+l_pizza_size: int = 25
+s_pepperoni: int = 2
+ml_pepperoni: int = 3
+cheese: int = 1
+bill:int = 0
+
+print("Python Pizza 배달에 오신 것을 환영합니다!")
+select_pizza_size: str = input("어떤 크기의 피자를 원하시나요? S, M, L \n")
+select_add_peppperoni: str = input("피자에 페퍼로니를 추가하시겠습니까? Y 또는 N \n")
+select_extra_cheese: str = input("추가 치즈를 원하시나요? Y 또는 N \n")
+
+if select_pizza_size == "S":
+    bill += s_pizza_size
+    if select_add_peppperoni == "Y":
+        bill += s_pepperoni
+    if select_extra_cheese == "Y":
+        bill += cheese
+elif select_pizza_size == "M":
+    bill += m_pizza_size
+    if select_add_peppperoni == "Y":
+        bill += ml_pepperoni
+    if select_extra_cheese == "Y":
+        bill += cheese
+else:
+    bill += l_pizza_size
+    if select_add_peppperoni == "Y":
+        bill += ml_pepperoni
+    if select_extra_cheese == "Y":
+        bill += cheese
+print(f"최종 결제 금액은 ${bill}입니다.")
+
+
+
+
+
