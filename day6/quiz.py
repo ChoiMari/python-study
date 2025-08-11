2b2eeb84900ba5491d1bf13f7afc529c937e71eb
ChoiMari <chl98639863@gmail.com>
2025-08-12 00:27:23 +0900

    feat : 퀴즈풀이

---
+++ day6/quiz.py
@@ -0,0 +1,22 @@
+# 퀴즈
+# 표준 체중을 구하는 프로그램을 작성하시오
+
+#[참고] 파이썬에서는 코드가 길어질 때 ()로 묶어서 줄바꿈 가능
+def std_weight(height:float, gender:str) -> float :
+    if (gender == "남자" 
+        or gender.upper() == "M" 
+        or gender.lower() == "man") :
+       return round(height * height * 22, 2)
+    elif (gender == "여자" 
+          or gender.upper() == "W" 
+          or gender.lower() == "woman") :
+        return round(height * height * 21, 2)
+    else :
+        raise ValueError(f"잘못된 입력: {gender}") # 예외 던짐
+
+height = 1.75
+gender = "남자"
+# :.1f 포맷지정자(소수점 1자리수로 출력)
+print(f"키 {height * 100:.1f}cm {gender}의 " 
+      f"표준 체중은 {std_weight(height, gender)}kg 입니다.")
+
