c128afca0d796444ee795962101b2e31b394320a
ChoiMari <chl98639863@gmail.com>
2026-06-02 18:17:00 +0900

    feat: 산술연산자

---
+++ 04_operator/arithmetic_operator.py
@@ -0,0 +1,17 @@
+# 산술 연산자
+a = 10
+b = 2
+# + 덧셈
+print(f"a + b = {a + b}")
+# - 뺄셈
+print(f"a - b = {a - b}")
+# * 곱셈
+print(f"a * b = {a * b}")
+# / 나눗셈 : 실수(float)타입으로 반환
+print(f"a / b = {a / b}")
+# // 몫
+print(f"a // b = {a // b}")
+# % 나머지
+print(f"a % b = {a % b}")
+# ** 거듭제곱: 10^2
+print(f"a ** b = {a ** b}")
