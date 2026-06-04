48998cc5ccfbd15f95ec611652d8e7fe6f08c46f
ChoiMari <chl98639863@gmail.com>
2026-06-04 17:20:18 +0900

    feat: 논리연산자

---
+++ 04_operator/logical_operator.py
@@ -0,0 +1,16 @@
+# 논리연산자
+# 특정 데이터를 가지고 논리적인 연산을 수행
+# 참인지 거짓인지 판단하는 연산
+# True 또는 False 값만 나옴
+# 논리 합 or
+# A or B : A, B 둘 중 1개라도 참이면 참
+print(True or False)  # True
+
+# 논리 곱 and
+# A and B : A와 B 둘 다 참이면 참
+print(True and True)  # True
+print(True and False)  # False
+
+# 논리 부정 not
+# not A : A가 True이면 거짓, 거짓이면 참
+print(not True)  # False
